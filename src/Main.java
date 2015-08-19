c78c1ce23a2358c1b58e7895136d9123b8771c0e
DesXApp <desxapp@gmail.com>
2015-08-20 00:27:23 +0800

    Initial project, including a example of abstract factory.
    Main.java:
    To test all examples of this project.
    
    designpattern\creational\AbstractFactory.java:
    An Abstract Factory class, including it's interface instance and a Simple Factory.

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import designpattern.creational.AbstractFactory;
+
+public class Main {
+    public static void main(String[] args) {
+    	// Run test abstract factory, and including simple factory.
+    	AbstractFactory.Test_AbstractFactory();
+    }
+}
---
+++ src/designpattern/creational/AbstractFactory.java
@@ -0,0 +1,81 @@
+package designpattern.creational;
+
+public class AbstractFactory {
+	// Implement abstracts factory instance and test paint a rectangle.
+	public static void Test_AbstractFactory() {
+    	AbstractFactory rectangle_part = new AbstractFactory();
+	
+    	// Implement a PointCornerFactory and implement these interfaces of Point and Corner.
+    	AbstractFactory.PointCornerFactory factory =
+            new AbstractFactory.PointCornerFactory() {
+                public AbstractFactory.Point getPoint() {
+                    return new AbstractFactory.Point() {
+                        public void line(int width) {
+                            for(int i = 0; i < width; i++) {
+                                System.out.print("-");
+                            }
+                        }
+                    };
+                }
+                
+                public AbstractFactory.Corner getCorner() {
+                    return new AbstractFactory.Corner() {
+                        public void leftUp() { System.out.print('+'); }
+                        public void rightUp() { System.out.print('+'); }
+                        public void leftDown() { System.out.print('+'); }
+                        public void rightDown() { System.out.print('+'); }
+                    };
+                }
+            };
+            rectangle_part.GetRectangle(20, 10).paint(factory);    
+    }
+	
+	// Like a simple factory.
+	Rectangle GetRectangle(int width, int height) {
+		return new Rectangle(width, height);
+	}
+	
+	interface PointCornerFactory {
+		Point getPoint();
+		Corner getCorner();
+	}
+
+	interface Point {
+		void line(int width);
+	}
+
+	interface Corner {
+		void leftUp();
+		void rightUp();
+		void leftDown();
+		void rightDown();
+	}
+
+	// Paint a rectangle via these interfaces.
+	class Rectangle {
+	    private int width;
+	    private int height;
+	    
+	    Rectangle(int width, int height) {
+	        this.width = width;
+	        this.height = height;
+	    }
+	    
+	    void paint(PointCornerFactory factory) {
+	        Point point = factory.getPoint();
+	        Corner corner = factory.getCorner();
+	        corner.leftUp();
+	        point.line(width - 2);
+	        corner.rightUp();
+	        System.out.println();
+	        for(int i = 0; i < height - 2; i++) {
+	            point.line(width);
+	            System.out.println();
+	        }
+	        corner.leftDown();
+	        point.line(width - 2);
+	        corner.rightDown();
+			System.out.println();
+		}
+	}
+}
